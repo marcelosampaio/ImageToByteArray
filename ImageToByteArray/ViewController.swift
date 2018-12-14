06ffac8a90c8d9436ac9c7014ff5df77fcd4e647
Marcelo Sampaio <marcelo@artlogica.com,br>
2018-12-14 09:38:27 -0200

    Initial Commit

---
+++ ImageToByteArray/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageToByteArray
+//
+//  Created by Marcelo Sampaio on 14/12/18.
+//  Copyright © 2018 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
